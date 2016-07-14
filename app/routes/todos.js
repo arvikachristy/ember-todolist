e9139bad774ec439bd1266ff54fa38a355461104
arvikachristy <anastasia.arvika@gmail.com>
2016-07-14 18:37:51 +0100

    Success update in the ember console :sailboat:

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 
 let tasks = [{
-	id: '1',
+	id: '10',
 	title: 'Get a  corgi',
 	description: 'at the pet store',
 	deadline: '12/09/2016'
 },{
-	id: '2',
+	id: '22',
 	title: 'Water the plant',
 	description: 'go back to the apartment',
 	deadline: '23/06/2016'
 }, {
-	id: '3',
+	id: '32',
 	title: 'Laundry day',
 	description: 'don\'t foget before sunday!',
 	deadline: '13/07/2016'
@@ -21,8 +21,9 @@ export default Ember.Route.extend({
 	model(){
 		// return this.get('store').findAll('todos');
 		var self = this;
+		var store = this.store;
 		tasks.forEach(function(task){
-	    	self.get('store').createRecord('todos', task);
+	    	store.createRecord('todos', task);	    	
 		});
 		return tasks;
 	}
